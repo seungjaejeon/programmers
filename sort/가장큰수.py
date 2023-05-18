b114756e55c1dc6faa98c43c61d99b235a3e6d00
seungjaejeon <wjstmdwo2000@naver.com>
2023-05-18 17:03:21 +0900

    sort

---
+++ sort/가장큰수.py
@@ -0,0 +1,31 @@
+def first_num(num):
+    while 1:
+        if num<10:
+            break;
+        else:
+            num//=10
+    return num
+def solution(numbers):
+    answer = ''
+    numbers.sort
+    max_num = 0
+    for i in numbers:
+        print(first_num(i))
+        if i >= 10:
+            if max_num < first_num(i):
+                max_num = i
+                
+        else:
+            if max_num<i:
+                max_num = i
+    answer += str(max_num)
+            
+    return answer
+
+
+# 0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요.
+
+# 예를 들어, 주어진 정수가 [6, 10, 2]라면 [6102, 6210, 1062, 1026, 2610, 2106]를 만들 수 있고, 이중 가장 큰 수는 6210입니다.
+
+# 0 또는 양의 정수가 담긴 배열 numbers가 매개변수로 주어질 때, 
+# 순서를 재배치하여 만들 수 있는 가장 큰 수를 문자열로 바꾸어 return 하도록 solution 함수를 작성해주세요.
