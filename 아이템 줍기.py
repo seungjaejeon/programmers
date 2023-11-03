0facab5bfb882cf52262193f3fa7afcc441c7928
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-03 21:17:27 +0900

    bfs와 구현을 사용한 아이템 줍기.py

--- 아이템 줍기.py
+++ 아이템 줍기.py
@@ -1,20 +1,18 @@
 from collections import deque
 def solution(rectangle, characterX, characterY, itemX, itemY):
     answer = 0
-    pan = [[0 for i in range(102)] for j in range(102)]
-    visited = [[0 for i in range(102)] for j in range(102)]
+    pan = [[-1 for i in range(102)] for j in range(102)]
+    visited = [[1 for i in range(102)] for j in range(102)]
     for val in rectangle:
         for i in range(val[0]*2, val[2]*2+1):
             for j in range(val[1]*2, val[3]*2+1):
-                pan[j][i] = 1
-    for val in rectangle:
-        for i in range(val[0]*2+1, val[2]*2):
-            for j in range(val[1]*2+1, val[3]*2):
-                pan[j][i] = 0 
+                if val[0]*2 < i < val[2]*2 and val[1]*2< j < val[3]*2:
+                    pan[j][i] = 0
+                elif pan[j][i]!=0:
+                    pan[j][i] = 1
     dx = [0,0,-1,1]
     dy = [1,-1,0,0]
     def bfs(x,y):
-        visited[x][y] = 1
         q = deque()
         q.append((x,y))
         while q:
@@ -24,16 +22,11 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
                 ny = b + dy[i]
                 if nx<0 or ny<0 or nx>=101 or ny>=101:
                     continue
-                if visited[nx][ny]==0 and pan[nx][ny]:
-                    visited[nx][ny]=1
-                    pan[nx][ny] += pan[a][b]
+                if visited[nx][ny]==1 and pan[nx][ny]==1:
+                    visited[nx][ny]+=visited[a][b]
                     q.append((nx,ny))
     
-    bfs(characterX*2, characterY*2)
-    answer = pan[itemX*2][itemY*2]
-    for i in range(30):
-        for j in range(30):
-            print(pan[i][j], end=" ")
-        print()
+    bfs(characterY*2, characterX*2)
+    answer = visited[itemY*2][itemX*2]//2
     
     return answer
